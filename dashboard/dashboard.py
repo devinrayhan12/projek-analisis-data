61d83136e3e08fe568fc6165c7e27d81439910ad
devinrayhan12 <devinrpa@gmail.com>
2026-04-28 12:15:15 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,6 +9,10 @@ st.header('Bike Sharing Analysis Dashboard 🚲')
 # Load data 
 df = pd.read_csv("dashboard/main_data.csv")
 
+if 'weather_label' not in df.columns:
+    weather_mapping = {1: 'Clear', 2: 'Misty', 3: 'Light Rain/Snow', 4: 'Heavy Rain'} [cite: 19, 20]
+    df['weather_label'] = df['weathersit'].map(weather_mapping) [cite: 19, 20]
+  
 # Tampilkan grafik 
 st.subheader("Penyewaan Berdasarkan Cuaca")
 fig, ax = plt.subplots()
